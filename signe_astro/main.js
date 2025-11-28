533abcc4ce180008a9cf3295e199b7e280ff56c7
Samy OUBOUAZIZ <soubouaziz@scaleway.com>
2025-11-28 11:25:20 +0100

    feat(js): add json

--- signe_astro/main.js
+++ signe_astro/main.js
@@ -1,3 +1,27 @@
 const birthDay = document.getElementById('birthDay');
 const birthMonth = document.getElementById('birthMonth');
+const submitButton = document.getElementById('submitButton');
 
+submitButton.addEventListener('click', function(){
+  if (birthDay.value === '') {
+    birthDay.style.border = "2px solid red";
+    birthDay.style.borderColor = "red";
+  }
+  
+  else if (birthMonth.value === '') {
+    birthMonth.style.border = "2px solid red";
+    birthMonth.style.borderColor = "red";
+  }
+
+  else {
+    // make calculation print result here
+  }
+});
+
+// convert 
+
+// if month < 9 and start in 1793
+// if month = 9 and day < 22 start in 1793
+// else start in 1792
+// 
+// calculate difference between entered date and first day of calendar
