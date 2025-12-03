9dedc1211dae7684a792a88dc56d3a8d4e442235
Samy O <samy.oubouaziz@gmail.com>
2025-12-03 09:40:15 +0100

    add day 2

---
+++ aoc2025/day2/main.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+// turn list into an array
+
+
+const lines = fs.readFileSync('input.txt', 'utf8').trim().split(',');
+
+// iterate over each item in array
+
+    // IMPORTANT - check if error is possible to avoid checking every value in range
+
+lines.forEach(line) => {
+    
+    
+    // check if id is invalid
+    
+    // split ID in half
+    
+    // get length of string
+    
+    // split at length/2
+    
+    // check if half1 = half2
+    
+    // if true, invalidTotal += invalid id (ternary operator) 
+    
+});
+
+
+// corner case ????
+
+console.log(lines);
